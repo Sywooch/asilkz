ae37d4472364494343b6e29a12f3c57cd279bbf9
Nurzhik <nurzhanbalkibek@gmail.com>
2015-09-02 18:22:12 +0600

    Добавил 2 модальное окно

--- web/themes/asil/modules/cms/widgets/views/feedback.php
+++ web/themes/asil/modules/cms/widgets/views/feedback.php
@@ -20,4 +20,14 @@ use \yii\widgets\ActiveForm;
     <?php ActiveForm::end()?>
 </div>
 <div id="overlay"></div>
+<div id="modal2" class="modal_div"> <!-- скрытый див с уникальным id = modal1 -->
+	<span class="modal_close"></span>
+	<span class="title_z">Обратный звонок</span>
+        <form method="POST" name="form1" action="form.php" >
+			<input  name="name1" id="name" maxlength="200" class="modal_f" type="text" size="1"  required placeholder="Имя..."/>
+			<input   name="phone1" id="user_phone" maxlength="200" class="modal_f " type="text" size="1"  required placeholder="Номер..."/>
+			<button type="submit" name="submit1" >Обратный Звонок</button>
+		</form>
+	</div>
+<div id="overlay"></div>
 <!-- Подложка, одна на всю страницу -->
