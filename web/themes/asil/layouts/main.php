ff31869b60661e175f6423d167fc0a350938b3c9
Sain <sainastana@gmail.com>
2015-09-15 17:28:33 +0600

    изменени главной фотки

--- web/themes/asil/layouts/main.php
+++ web/themes/asil/layouts/main.php
@@ -15,7 +15,7 @@ $currentUrl = Yii::$app->request->url;
 <html lang="<?= Yii::$app->language ?>">
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=1020">
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
